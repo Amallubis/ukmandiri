f8e3f9dc1605597bc46c0875b8bcafc8a46320ef
Amallubis <amal.lubis@gmail.com>
2023-09-12 13:24:10 +0700

    menambahkan filemember

--- configweb/member/forms.py
+++ configweb/member/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
+from django.contrib.admin import widgets
 from member.models import Member
 
 class FormAddMember(ModelForm):
     class Meta:
         model = Member
         fields = '__all__'
+        
+        widgets ={
+            'tanggal_lahir':widgets.AdminDateWidget()
+            
+        }
 
